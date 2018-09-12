723406831fa604fa937be02e24b9f6021f081747
Mike Erickson <codedungeon@gmail.com>
2018-09-12 16:33:42 -0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <VueTable name="mike erickson" dob="10-15-1966"/>
+  </div>
+</template>
+
+<script>
+  import VueTable from "./components/VueTable.vue";
+
+  export default {
+    name: "app",
+    components: {
+      VueTable
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/VueTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>Hello <strong>{{this.titleCase(name)}} (DOB: {{dob}} Age: {{calcAge}})</strong>, how are you doing today?</p>
+    <button @click="sayHello()"> > Show Hello Badge < </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: String,
+      dob: String
+    },
+    methods: {
+      calculateAge(dateString) {
+        return ~~((Date.now() - new Date(dateString)) / 31557600000);
+      },
+      titleCase(str) {
+        return str
+          .toLowerCase()
+          .split(" ")
+          .map(function(word) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+          })
+          .join(" ");
+      },
+      sayHello() {
+        alert("Hello My Name Is " + this.name);
+      }
+    },
+    computed: {
+      calcAge() {
+        return this.calculateAge(this.dob);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+</style>
